caaf88e15236c41fc7c4b522bfed0c812f6e5a14
Sergio Casas <sac.9885@gmail.com>
2023-01-24 19:31:02 -0500

    add names to files

--- cositas.py
+++ cositas.py
@@ -1,14 +1,15 @@
 
 class Archivo:
 
-    def __init__(self, weight, type, lenguage):
+    def __init__(self, weight, type, lenguage, name):
         self.weight = weight
         self.type = type
         self.lenguage = lenguage
+        self.name = name
 
     
     def __str__(self):
-        s = f"Program lenguage: {self.lenguage}\nType of file: {self.type}\nWeight: {self.weight}\n"
+        s = f"{self.name}\nProgram lenguage: {self.lenguage}\nType of file: {self.type}\nWeight: {self.weight}\n"
         return s
 
 
@@ -18,8 +19,9 @@ class Directory(Archivo):
         self.list = []
         
 
-    def add_file(self, weight, type, lenguage):
-        file = Archivo(weight, type, lenguage)
+    def add_file(self, name, weight, type, lenguage):
+        file = Archivo(name, weight, type, lenguage)
+        file.name = name
         file.lenguage = lenguage
         file.type = type
         file.lenguage = lenguage
@@ -41,8 +43,8 @@ class Directory(Archivo):
 
 development = Directory()
 
-development.add_file(12,"TXT","Python")
-development.add_file(23,"TXT","C++")
-development.add_file(43,"ORG","Js")
+development.add_file("juego.py",12,"TXT","Python")
+development.add_file("algoritmo.cpp", 23,"TXT","C++")
+development.add_file("web.js", 43,"ORG","Js")
 development.user("pedro")
 development.__str__()
